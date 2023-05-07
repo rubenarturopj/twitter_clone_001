19566754232e4b10a7782cdfdcc1bc2b6c0b6099
Ruben Perez <ruben.perezjuarez@mail.mcgill.ca>
2023-05-07 16:24:30 -0400

    like follow hook and control created

--- 01_Instructions/08_like_and_follow_functionality.md
+++ 01_Instructions/08_like_and_follow_functionality.md
@@ -1,5 +1,7 @@
-## Under construction
-
-## 👷‍♂️🏗️🚧⚠️
-
 ## Like and follow functionality
+
+1. Go to `pages/api/` folder and create a new file `follow.ts`. Type the necessary functions to handle when start following someone and then stop following that user.
+
+2. Now we need a hook for this. So, go to `hooks` folder and create a new file called `useFollow.ts`. Write the file and make sure to export it at the bottom.
+
+3. Go to `components/user/UserBio.tsx`.
---
+++ pages/api/follow.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@/libs/prismadb";
+import serverAuth from "@/libs/serverAuth";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    // for the follow function, we will POST to add a person you want to follor
+    //  or DELETE to delete the person from your "following" list
+    if (req.method !== "POST" && req.method !== "DELETE") {
+        return res.status(405).end();
+    }
+
+    try {
+        const { userId } = req.body;
+
+        const { currentUser } = await serverAuth(req, res);
+
+        if (!userId || typeof userId !== "string") {
+            throw new Error("Invalid ID");
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            throw new Error("Invalid ID");
+        }
+
+        let updatedFollowingIds = [...(user.followingIds || [])];
+
+        // we are following someone
+        if (req.method === "POST") {
+            updatedFollowingIds.push(userId);
+        }
+
+        // if we want to stop following someone
+        if (req.method === "DELETE") {
+            updatedFollowingIds = updatedFollowingIds.filter(
+                (followingId) => followingId !== userId
+            );
+        }
+
+        // update the user info with the changes we just did before
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: currentUser.id,
+            },
+            data: {
+                followingIds: updatedFollowingIds,
+            },
+        });
+
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end();
+    }
+}
